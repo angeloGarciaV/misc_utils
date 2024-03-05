37653f472f5019336dd9880501a9c6f7efd2e6b2
Angelo Garcia <64263025+angeloGarciaV@users.noreply.github.com>
2024-03-05 16:16:14 -0400

    Added functionality to completely change file names

--- dup.py
+++ dup.py
@@ -1,19 +1,29 @@
 import os
 import sys
+from re import split
 
 
-def duplicate_file(file, num=1):
+def duplicate_file(file, num=1, *args):
     print("============")
     if len(sys.argv) >= 3:
         num = int(sys.argv[2])
+        new_name = sys.argv[3]
+    elif len(sys.argv) >= 4:
+        new_type = sys.argv[4]
 
-    file_name_split = file.split("-")
+    file_name_split = split("-|\.", file)
+    print(file_name_split)
+    file_type = file_name_split[2]
     old_file_num = int(file_name_split[0])
+
     num += old_file_num
     sum = num
     new_file_num = str(sum)
     file_name_split[0] = new_file_num
-    new_file_name = "-".join(file_name_split)
+
+    if len(sys.argv) >= 3:
+        file_name_split[1] = new_name
+    new_file_name = f"{file_name_split[0]}-{file_name_split[1]}.{file_type}"
 
     if not os.path.isfile(file):
         print(f"{file} does not exist.")
