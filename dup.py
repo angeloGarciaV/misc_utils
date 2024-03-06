bce55e9f6d33f1897e562dd34cda4a894fa2954f
Angelo Garcia <64263025+angeloGarciaV@users.noreply.github.com>
2024-03-05 21:10:33 -0400

    Update dup.py

--- dup.py
+++ dup.py
@@ -4,26 +4,27 @@ from re import split
 
 
 def duplicate_file(file, num=1, *args):
+    new_name = None
     print("============")
-    if len(sys.argv) >= 3:
+    if len(sys.argv) == 3 and sys.argv[2].isdigit():
         num = int(sys.argv[2])
-        new_name = sys.argv[3]
-    elif len(sys.argv) >= 4:
-        new_type = sys.argv[4]
+    elif len(sys.argv) == 4 and sys.argv[2].isdigit():
+        num = int(sys.argv[2])
+        new_name = str(sys.argv[3])
+    elif len(sys.argv) == 3 and not sys.argv[2].isdigit():
+        new_name = str(sys.argv[2])
 
     file_name_split = split("-|\.", file)
-    print(file_name_split)
-    file_type = file_name_split[2]
     old_file_num = int(file_name_split[0])
+    name = file_name_split[1]
+    file_type = file_name_split[2]
 
-    num += old_file_num
-    sum = num
-    new_file_num = str(sum)
+    new_file_num = num + old_file_num
     file_name_split[0] = new_file_num
+    if new_name is not None:
+        name = new_name
 
-    if len(sys.argv) >= 3:
-        file_name_split[1] = new_name
-    new_file_name = f"{file_name_split[0]}-{file_name_split[1]}.{file_type}"
+    new_file_name = f"{new_file_num}-{name}.{file_type}"
 
     if not os.path.isfile(file):
         print(f"{file} does not exist.")
